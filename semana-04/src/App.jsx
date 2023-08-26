c9d3624a6b849fa89899c971840b5f91de8a8183
Shair17 <jimmy_jhair17@hotmail.com>
2023-08-26 13:12:44 -0500

    hackaton hooks

--- semana-04/src/App.jsx
+++ semana-04/src/App.jsx
@@ -1,13 +1,53 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+// Hackaton (HOOKS):
+// 1. Hacer un contador con los operadores de (+, -, *, /, %) y para cada uno de ellos crear una función por aparte
+// 2. Hacer un reloj y que se actualice cada segundo, usar useEffect y useState
+// 3. Hacer una lista de tareas, usar useState y cada que se actualicen las tareas mostrarlas por consola además de la interfaz
+
+function ContadorBoton() {
+	const [contador, setContador] = useState(0);
+
+	useEffect(() => {
+		console.log('El componente se montó');
+
+		return () => {
+			console.log('El componente se desmontó');
+		};
+	}, []);
+
+	useEffect(() => {
+		console.log('el contador aumentó', contador);
+	}, [contador]);
+
+	const manejarAumento = () => {
+		setContador(contador + 1);
+	};
+
+	return (
+		<div>
+			<h1>Contador: {contador}</h1>
+
+			<button onClick={manejarAumento}>Aumentar +1</button>
+		</div>
+	);
+}
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [esVisible, setEsVisible] = useState(true);
 
 	return (
 		<div className="contenedor">
-			<h1>Mi proyecto con ReactJS</h1>
+			<button
+				style={{ marginBottom: 100 }}
+				onClick={() => {
+					setEsVisible(!esVisible);
+				}}
+			>
+				{esVisible ? 'Ocultar' : 'Mostrar'} contador
+			</button>
 
-			<img src="https://source.unsplash.com/random/?night" alt="Imagen" />
+			{esVisible ? <ContadorBoton /> : null}
 		</div>
 	);
 }
